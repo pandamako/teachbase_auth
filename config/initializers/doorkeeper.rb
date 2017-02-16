c1004423e5f0180afbd91d5ebe30b23308d104d6
Artem Bykov <artem.bikov@gmail.com>
2017-02-16 22:28:56 +0300

    auto authorize application

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -107,6 +107,9 @@ Doorkeeper.configure do
   # skip_authorization do |resource_owner, client|
   #   client.superapp? or resource_owner.admin?
   # end
+  skip_authorization do |resource_owner, client|
+    true
+  end
 
   # WWW-Authenticate Realm (default "Doorkeeper").
   # realm "Doorkeeper"
